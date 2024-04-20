fd3d4847d5eb86459216f3b80ba9cd25dd27faa3
Tomáš <159623262+jasomtubratu@users.noreply.github.com>
2024-04-20 12:46:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import datetime
 from utils.utils import clear_console, change_cmd_title, print_blue, check_ffmpeg_installation
 from utils.http_requests import send_herohero_request
 from utils.file_handling import fetch_herohero_user, prompt_herohero_credentials
